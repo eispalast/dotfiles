43718759bf313ac7bae2132a3940ed35ee2f2106
Timo Schlegel <timo.schlegel@campus.tu-berlin.de>
2022-07-31 11:48:15 +0200

    explained wallpaperpath

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -7,7 +7,6 @@ run() {
   fi
 }
 
-# run "compton"
 run "xrandr --output eDP-1 --mode 2256x1504 --set 'scaling mode' 'None' --pos 0x0 --rotate normal --rate 60 --dpi 144"
 run "keepassxc"
 picom --config ~/.config/picom/picom.conf -b
@@ -16,7 +15,7 @@ picom --config ~/.config/picom/picom.conf -b
 # nitrogen --restore
 
 #compton --backend glx --paint-on-overlay --glx-no-stencil --vsync opengl-swc --unredir-if-possible &
-nextcloud=$(ls ~/software | grep Nextcloud | tail -n 1)
-~/software/$nextcloud &
+nextcloud=$(ls ~/exchange/software | grep Nextcloud | tail -n 1)
+~/exchange/software/$nextcloud &
 
 ~/.config/awesome/startkmonad.sh
--- .config/awesome/mytheme/theme.lua
+++ .config/awesome/mytheme/theme.lua
@@ -98,6 +98,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
+-- put your wallpaper here or chose another path (up top)
 theme.wallpaper = wallpaper_path.."wallpaper.jpg"
 
 -- You can use your own layout icons like this:

831cd9f94dda31aac8467f22352ff42c7abfedee
Timo Schlegel <timo.schlegel@campus.tu-berlin.de>
2023-02-09 13:10:16 +0100

    deleted my own increment function, because TIL there is one build in

--- .config/nvim/lua/tools.lua
+++ .config/nvim/lua/tools.lua
@@ -3,26 +3,6 @@ vim.keymap.set('i','jk','<ESC>')
 --quick save
 vim.keymap.set({'n','i'},'<C-s>', '<ESC>:w<CR>')
 
--- Increase and decrese numbers
-function count(updown)
-    local current_word = vim.call('expand','<cword>')
-    local as_num = tonumber(current_word)
-    if as_num then
-        if updown == '+' then
-            as_num = as_num+1
-        elseif updown == '-' then
-            as_num = as_num-1
-        else
-            return
-        end
-        return "<ESC>diwa" .. as_num .. "<ESC>"
-    else
-        print("no number")
-    end
-end
-vim.keymap.set('n','+', function() return count("+") end,{ expr=true })
-vim.keymap.set('n','-', function() return count("-") end,{ expr=true })
-
 --Debugging
 vim.keymap.set('n', '<leader>dd', ':call vimspector#Launch()<CR>',{ silent=true})
 vim.keymap.set('n', '<leader>db', ':make<CR> :call vimspector#Launch()<CR>',{ silent=true})

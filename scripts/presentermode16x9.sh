9a9c2e473466b63f6e0c90a30ba1b44fb375d544
Timo Schlegel <timo.schlegel@campus.tu-berlin.de>
2023-09-08 08:42:17 +0200

    new wording in xinput

--- scripts/presentermode16x9.sh
+++ scripts/presentermode16x9.sh
@@ -3,10 +3,10 @@
 xrandr --output eDP-1 --mode 1920x1080
 xrandr --output HDMI-1 --mode 1920x1080 --same-as eDP-1
 
-touchscreen=$(xinput list | grep "Pen Pen" | sed 's/.*id=//' | cut -d "[" -f 1 | tr -d -c 0-9)
+touchscreen=$(xinput list | grep "Pen stylus" | sed 's/.*id=//' | cut -d "[" -f 1 | tr -d -c 0-9)
 touchscreen_area=$(xinput list-props $touchscreen | grep "Coordinate Transformation Matrix (" | cut -d ")" -f 1 | tr -d -c 0-9)
 
-eraser=$(xinput list | grep "Pen Eraser" | sed 's/.*id=//' | cut -d "[" -f 1 | tr -d -c 0-9)
+eraser=$(xinput list | grep "Pen eraser" | sed 's/.*id=//' | cut -d "[" -f 1 | tr -d -c 0-9)
 finger=$(xinput list | grep "Finger" | sed 's/.*id=//' | cut -d "[" -f 1 | tr -d -c 0-9)
 xinput set-prop $touchscreen $touchscreen_area 1.0, 0.0, 0.0, 0.0, 1.18, -0.09, 0.0, 0.0, 1.0
 

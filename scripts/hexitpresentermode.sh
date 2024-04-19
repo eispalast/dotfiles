b2f1230246cdafeee9d43e1f812e9add5da63a0a
Timo Schlegel <timo.schlegel@campus.tu-berlin.de>
2024-04-19 10:08:13 +0200

    updated hyprland config after their update

--- scripts/hexitpresentermode.sh
+++ scripts/hexitpresentermode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-hyprctl keyword monitor eDP-1,2258x1504,auto,1.566667
+hyprctl keyword monitor eDP-1,2256x1504,auto,1.566667
 hyprctl keyword monitor HDMI-A-1,preferred,auto,1
-hyprctl keyword device:wacom-hid-495f-pen:transform 0
-hyprctl keyword device:wacom-hid-495f-finger:transform 0
+hyprctl keyword "device[wacom-hid-495f-pen]:transform" 0
+hyprctl keyword "device[wacom-hid-495f-finger]:transform" 0
 
